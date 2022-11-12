ec49d3ef921efc9b5c510e6eeba0b13d2434d725
mounikamummadi97 <mounikamummadi1206@gmail.com>
2022-11-12 12:01:50 +0530

    Add project files.

---
+++ Test2_Mounika_Method_Overloading_Sum/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Test2_Mounika_Method_Overloading_Sum;
+
+SumCalculation objSumCalculation = new SumCalculation();
+
+objSumCalculation.Calculation(10, 20);
+objSumCalculation.Calculation(20, 30,50);
---
+++ Test2_Mounika_Method_Overloading_Sum/SumCalculation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test2_Mounika_Method_Overloading_Sum
+{
+    public class SumCalculation
+    {
+        public  void Calculation(int _firstNumber, int _secondNumber)
+        {
+            int sum = _firstNumber + _secondNumber;
+            Console.WriteLine("Sum of Two numbers :"+ sum);
+        }
+        public  void Calculation(int _firstNumber, int _secondNumber,int _thirdNumber)
+        {
+            int sum = _firstNumber + _secondNumber + _thirdNumber;
+            Console.WriteLine("Sum of Three numbers :" + sum);
+        }
+    }
+    
+}
